588d44d2bbf7d54aecf0bdb9e80dbe27a7d3c8d1
Giliard Silva <ghssilva248@gmail.com>
2024-12-11 12:28:39 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 function toggleMenu() {
-    const navMenu = document.getElementById('nav-menu');
-    navMenu.classList.toggle('active');
-    console.log('Menu toggled', navMenu.classList); 
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.toggle('active');
+  console.log('Menu toggled', navMenu.classList); 
+  
 }
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const observerOptions = {
+    threshold: 0.2, 
+  };
+  
+  const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        } else {
+          entry.target.classList.remove("show");
+        }
+      });
+    }, observerOptions);
+    
+    const hiddenElements = document.querySelectorAll(".hidden");
+    hiddenElements.forEach((el) => observer.observe(el));
+  });
+  
+  
+  
+ 
